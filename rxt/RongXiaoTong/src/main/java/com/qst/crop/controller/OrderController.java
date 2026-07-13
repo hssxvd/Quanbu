4e1708b3d264e5a62c7a540666efeef4594bc23f
whiteDew <2797366870@qq.com>
2026-07-13 16:49:36 +0800

    feat: 实现个人销售订单查询相关接口

--- rxt/RongXiaoTong/src/main/java/com/qst/crop/controller/OrderController.java
+++ rxt/RongXiaoTong/src/main/java/com/qst/crop/controller/OrderController.java
@@ -1,8 +1,12 @@
 package com.qst.crop.controller;
 
 import com.qst.crop.common.Result;
+import com.qst.crop.common.StatusCode;
+import com.qst.crop.entity.SellPurchase;
 import com.qst.crop.model.MyPurchase;
 import com.qst.crop.service.PurchaseService;
+import com.qst.crop.service.SellPurchaseService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,9 +23,19 @@ public class OrderController {
     @Autowired
     private PurchaseService purchaseService;
 
+    @Autowired
+    private SellPurchaseService sellPurchaseService;
+
     @GetMapping("/selectBuys")
     public Result<List<MyPurchase>> selectBuys() {
         List<MyPurchase> purchases = purchaseService.selectBuys();
         return new Result<>(true, 20000, "查询成功", purchases);
+    }
+
+    @Operation(summary = "根据登录用户查询我卖出的订单")
+    @GetMapping("/selectSells")
+    public Result<List<SellPurchase>> selectSells() {
+        List<SellPurchase> purchase = sellPurchaseService.selectByName();
+        return new Result<>(true, StatusCode.OK, "查询成功", purchase);
     }
 }
